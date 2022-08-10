75d246fc04f63caec048f48e4696393098729d6c
hamzahjomaa <hamzah.m.jomaa@gmail.com>
2022-08-10 21:08:36 +0300

    wizard

--- Project02/assets/js/wizard.js
+++ Project02/assets/js/wizard.js
@@ -20,9 +20,7 @@ $(document).ready(function() {
 
     let step = 0
 
-    if (step  === 0)
-        $("#back").hide()
-
+    $("#back").hide()
     $("#titleQuestion").text(questions[4].question)
     $(".answers.section").css("gap", "2rem")
 
@@ -38,8 +36,15 @@ $(document).ready(function() {
         `)
     })
 
-    $("body").on("click","#next",function(){
-        step++
+
+    const updateQuestion = (direction) =>{
+        if (direction == "next")
+            step++
+        else
+            step--
+            
+        if (step != 0)
+            $("#back").show()
         $("#progressBar").css("width", `${(step / questions.length) * 100}%`)
         $("#titleQuestion").text(questions[step].question)
         $(".answers.section").empty()
@@ -77,10 +82,56 @@ $(document).ready(function() {
                     `)
                 }
             })
+        }else if (questions[step].type == "form" ) {
+
+            $(".answers.section").empty().css("flex-direction", "column")
+            $(".answers.section").append(`
+            <div class="col-xl-12">
+                <div class="form-input">
+                    <fieldset class='float-label-field'>
+                        <label for="insuranceCompanyName" >First Name</label>
+                        <input id="insuranceCompanyName" type='text' name="insuranceCompanyName">
+                    </fieldset>
+                </div>
+            </div>
+            <div class="col-xl-12">
+                <div class="form-input">
+                    <fieldset class='float-label-field'>
+                        <label for="insuranceCompanyName" >Last Name</label>
+                        <input id="insuranceCompanyName" type='text' name="insuranceCompanyName">
+                    </fieldset>
+                </div>
+            </div>
+            <div class="col-xl-12">
+                <div class="form-input">
+                    <fieldset class='float-label-field'>
+                        <label for="insuranceCompanyName" >Email</label>
+                        <input id="insuranceCompanyName" type='text' name="insuranceCompanyName">
+                    </fieldset>
+                </div>
+            </div>
+            <div class="col-xl-12">
+                <div class="form-input">
+                    <fieldset class='float-label-field'>
+                        <label for="insuranceCompanyName" >Mobile</label>
+                        <input id="insuranceCompanyName" type='text' name="insuranceCompanyName">
+                    </fieldset>
+                </div>
+            </div>
+            <div class="col-xl-12">
+                <div class="form-input">
+                    <fieldset class='float-label-field'>
+                        <label for="insuranceCompanyName" >Nationality</label>
+                        <input id="insuranceCompanyName" type='text' name="insuranceCompanyName">
+                    </fieldset>
+                </div>
+            </div>
+            `)
+            
         }else{
             $(".answers.section").css("flex-direction", "column")
 
-            let progress = `<div class="card ${questions[step].type} border-0 w-100">
+            let progress = `<div class="card ${questions[step].type} border-0 w-100 slider-container">
                                 <input id="slider" class="slider" type="range" min="${questions[step].answers[0].value}" max="${questions[step].answers[1].value}" value="${(questions[step].answers[1].value - questions[step].answers[0].value)/2}">
                                 <span class="bar"><span class="fill"></span></span>
                             </div>`
@@ -95,7 +146,28 @@ $(document).ready(function() {
         
             $(`.answers.section`).append(input, progress, label)
         }
+    }
 
+    $("body").on("click","#back",()=>{
+        updateQuestion("back")
     })
+
+    $("body").on("click","#next",()=>{
+        updateQuestion("next")
+    })
+
+
+    $('body').on('focus',"input", function(event) {
+        $(this).closest('.float-label-field').addClass('float').addClass('focus');
+    })
+
+    $('body').on('blur',"input", function(event) {
+        $(this).closest('.float-label-field').removeClass('focus');
+        if (!$(this).val()) {
+        $(this).closest('.float-label-field').removeClass('float');
+        }
+    });
     
 })
+
+
